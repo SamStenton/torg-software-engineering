fb2ae12239076503f5ca5f1c90e3a9e0cd9cda47
Samuel Stenton <sthstenton@gmail.com>
2017-02-17 20:45:26 +0000

    Add logout

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -44,7 +44,7 @@
                     <li><i class="fa fa-user fa-2x" aria-hidden="true"></i><a href="">Profile</a></li>
                     <li><i class="fa fa-comments-o fa-2x" aria-hidden="true"></i><a href="">Messages</a></li>
                     <li><i class="fa fa-sliders fa-2x" aria-hidden="true"></i><a href="">Settings</a></li>
-                    <li><i class="fa fa-sign-out fa-2x" aria-hidden="true"></i><a href="">Logout</a></li>
+                    <li><i class="fa fa-sign-out fa-2x" aria-hidden="true"></i><a href="/logout">Logout</a></li>
                 </ul>
             </nav>
 
